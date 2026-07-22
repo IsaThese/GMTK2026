c9c402318dbe0c7d8a68839931c169de33089ae2
Isaac <isaacalvarado222@gmail.com>
2026-07-22 14:07:22 -0400

    SoundManager and Main

--- scripts/global/SoundManager.gd
+++ scripts/global/SoundManager.gd
@@ -1,1 +1,19 @@
 extends Node
+
+var sounds: Dictionary = {}
+
+enum SoundID {
+	Bomb,
+	Boom,
+	Click,
+	UiClick,
+	Poof,
+}
+
+func _ready() -> void:
+	sounds[SoundID.Bomb] = "res://sounds/bomb.ogg"
+	sounds[SoundID.Click] = "res://sounds/click.wav"
+
+
+func register() -> void:
+	pass
