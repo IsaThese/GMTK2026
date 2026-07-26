9e810a270d7ddc2517677afab95e065e627f407a
Zeroji <zzeroji@gmail.com>
2026-07-26 18:43:59 +0200

    Add real title screen

--- scripts/title.gd
+++ scripts/title.gd
@@ -1,8 +1,15 @@
 extends Control
 
+
+@onready var bike_enabled = Controls.TotalFails >= Controls.BikeUnlock
+
+
 func _ready() -> void:
 	$Settings.hide()
 	$CreditsLabel.hide()
+	
+	$BikeLock.visible = !bike_enabled
+	$BikeButton.visible = bike_enabled
 
 
 func _on_start_button_pressed() -> void:
@@ -15,3 +22,13 @@ func _on_settings_button_pressed() -> void:
 
 func _on_credits_button_pressed() -> void:
 	$CreditsLabel.visible = !$CreditsLabel.visible
+
+
+func _on_car_button_pressed() -> void:
+	Controls.BikeCity = false
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_bike_button_pressed() -> void:
+	Controls.BikeCity = true
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
