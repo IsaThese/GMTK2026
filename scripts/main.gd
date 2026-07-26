f36d1378878ff8b51ee7fe4f9750d4a970d90c84
Zeroji <zzeroji@gmail.com>
2026-07-26 12:30:02 +0200

    Add new assets, change entire map

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
-@onready var zone: Node2D = $Zone1
+@onready var zone: Node2D = $Zone2
 @onready var customer: Node2D = $Customer
 
 func _ready() -> void:
+	# Change clear color to sidewalk
+	RenderingServer.set_default_clear_color(Color("#949494"))
+	
 	var customer_positions = zone.get_node("CustomerPositions")
 	if customer_positions == null or customer_positions.get_child_count() == 0:
 		push_warning("No customer!")
